0088ad5377d36f1a53000e2a7c648be646eacb6c
Elias <hologamer7788@gmail.com>
2024-02-18 21:07:41 +0100

    Initial commit

---
+++ src/main/java/elias/calcu/Calcu.java
@@ -0,0 +1,16 @@
+package elias.calcu;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Calcu implements ModInitializer {
+	public  static final String MOD_ID = "calcu";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/elias/calcu/CalcuClient.java
@@ -0,0 +1,10 @@
+package elias.calcu;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CalcuClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
